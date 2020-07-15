8ac016f9361e482cea4d70ceb11bc35a12050bda
fT5SgrjXeV8khTNVgQmD <4913771+y9pQnhrdix7HkhonyMXiLSo9@users.noreply.github.com>
2020-07-15 12:13:21 -0500

    Add -Install for new PSWindowsUpdate Module

--- Maintenance_Script.ps1
+++ Maintenance_Script.ps1
@@ -1,16 +1,15 @@
 ##Install and Import AD Modules For Powershell v6
-Install-Module -Name WindowsCompatibility
 Import-Module -Name WindowsCompatibility 
 Import-WinModule -Name ActiveDirectory
 
 #Install and Import Powershell AD Module
-Install-WindowsFeature RSAT-AD-PowerShell
 Import-Module ActiveDirectory
 
 $updatelist = Get-ADComputer -Filter * | Select -ExpandProperty Name
 
 #####Install Latest Windows Updates#####
-Invoke-Command -Computer $updatelist -ScriptBlock {Set-ExecutionPolicy Unrestricted; Import-Module PSWindowsUpdate; Get-WUInstall –AcceptAll -Verbose -IgnoreReboot} -asjob
+Invoke-Command -Computer $updatelist -ScriptBlock {Set-ExecutionPolicy Unrestricted; Import-Module PSWindowsUpdate; #Add-WUServiceManager -ServiceID 7971f918-a847-4430-9279-4a52d1efe18d; Get-WUInstall -AcceptAll -Install –MicrosoftUpdate -Verbose -IgnoreReboot} 
 
 ##Schedule Update for EoD
-Invoke-Command -Computer $updatelist -ScriptBlock {$time = "20:00:00" ; $date = "02/4/2019" ; schtasks /create /tn “Scheduled Reboot” /tr “shutdown /r /t 0” /sc once /st $time /sd $date /ru “System”}
+Invoke-Command -Computer $updatelist -ScriptBlock {$time = "20:00:00" ; $date = "07/15/2020" ; schtasks /create /tn "ScheduledReboot20200715" /tr "Shutdown -r -t 0" /sc once /st $time /sd $date /ru "System"}
+
