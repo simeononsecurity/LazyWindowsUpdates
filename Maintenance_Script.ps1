d2e9305f856468808d3b87079add8949bcd91f18
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2020-12-16 17:38:49 -0600

    Update Maintenance_Script.ps1

--- Maintenance_Script.ps1
+++ Maintenance_Script.ps1
@@ -5,13 +5,22 @@ Import-WinModule -Name ActiveDirectory
 #Install and Import Powershell AD Module
 Import-Module ActiveDirectory
 
-$updatelist = Get-ADComputer -Filter * | Select -ExpandProperty Name
+$updatelist = (Get-ADComputer -Filter *).Name
 
 #####Install Latest Windows Updates#####
-Invoke-Command -Computer $updatelist -ScriptBlock {Set-ExecutionPolicy Unrestricted; Import-Module PSWindowsUpdate; 
-Add-WUServiceManager -ServiceID 7971f918-a847-4430-9279-4a52d1efe18d;
-Get-WUInstall -AcceptAll -Install –MicrosoftUpdate -Verbose -IgnoreReboot} 
+ForEach ($Computer in $updatelist){
+    Invoke-Command -Computer $Computer -ScriptBlock {
+        Set-ExecutionPolicy Unrestricted; Import-Module PSWindowsUpdate; 
+        Add-WUServiceManager -ServiceID 7971f918-a847-4430-9279-4a52d1efe18d -Confirm:$false;
+        Get-WUInstall -AcceptAll -Install –MicrosoftUpdate -Verbose -IgnoreReboot
+        Install-WindowsUpdate -AcceptAll -MicrosoftUpdate -IgnoreReboot
+        #Get-WUInstall –MicrosoftUpdate –AcceptAll –AutoReboot
+        #Install-WindowsUpdate -AcceptAll -MicrosoftUpdate -AutoReboot
+        } 
+    Invoke-Command -Computer $Computer -ScriptBlock {
+        $time = "18:00:00" ; $date = "12/16/2020" ;
+        schtasks /create /tn "ScheduledReboot20200715" /tr "Shutdown -r -t 0" /sc once /st $time /sd $date /ru "System"
+        }
 
-##Schedule Update for EoD
-Invoke-Command -Computer $updatelist -ScriptBlock {$time = "20:00:00" ; $date = "07/15/2020" ; schtasks /create /tn "ScheduledReboot20200715" /tr "Shutdown -r -t 0" /sc once /st $time /sd $date /ru "System"}
+}
 
