b4e103408e8632df285ff745cd1aed49cdaee3f4
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2020-12-16 17:11:13 -0600

    Update Maintenance_Script.ps1

--- Maintenance_Script.ps1
+++ Maintenance_Script.ps1
@@ -8,7 +8,9 @@ Import-Module ActiveDirectory
 $updatelist = Get-ADComputer -Filter * | Select -ExpandProperty Name
 
 #####Install Latest Windows Updates#####
-Invoke-Command -Computer $updatelist -ScriptBlock {Set-ExecutionPolicy Unrestricted; Import-Module PSWindowsUpdate; #Add-WUServiceManager -ServiceID 7971f918-a847-4430-9279-4a52d1efe18d; Get-WUInstall -AcceptAll -Install –MicrosoftUpdate -Verbose -IgnoreReboot} 
+Invoke-Command -Computer $updatelist -ScriptBlock {Set-ExecutionPolicy Unrestricted; Import-Module PSWindowsUpdate; 
+Add-WUServiceManager -ServiceID 7971f918-a847-4430-9279-4a52d1efe18d;
+Get-WUInstall -AcceptAll -Install –MicrosoftUpdate -Verbose -IgnoreReboot} 
 
 ##Schedule Update for EoD
 Invoke-Command -Computer $updatelist -ScriptBlock {$time = "20:00:00" ; $date = "07/15/2020" ; schtasks /create /tn "ScheduledReboot20200715" /tr "Shutdown -r -t 0" /sc once /st $time /sd $date /ru "System"}
